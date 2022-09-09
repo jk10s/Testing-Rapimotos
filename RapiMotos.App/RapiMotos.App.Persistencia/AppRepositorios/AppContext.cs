3ffa04fcb280ac5743859ed57dab5512aea7580a
Eliclavijo <eli.13.07@hotmail.com>
2022-09-08 20:48:43 -0500

    Avance proyecto Rapi Motos (capa consola, parte de migraciones, referencias)
    Avance proyecto Rapi Motos (capa consola, parte de migraciones, referencias)

---
+++ RapiMotos.App/RapiMotos.App.Persistencia/AppRepositorios/AppContext.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RapiMotos.App.Dominio;
+using Microsoft.EntityFrameworkCore; 
+
+namespace RapiMotos.App.Persistencia
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Persona> Persona { get; set; }
+        public DbSet<Administrador> Administrador { get; set; }
+        public DbSet<HistorialCliente> HistorialCliente { get; set; }
+        public DbSet<HitorialTecnico> HitorialTecnico { get; set; }
+        public DbSet<Servicio> Servicio { get; set; }
+        public DbSet<Tecnico> Tecnico { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<TipoServicio> TipoServicio { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if(!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder
+            .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = RapiMotos ");
+        }
+    }   
+    }
+}
---
+++ RapiMotos.App/RapiMotos.App.Persistencia/Migrations/20220909004429_Inicial.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RapiMotos.App.Persistencia.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HistorialCliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HistorialCliente", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HitorialTecnico",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DescripcionMantenimiento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaHora = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HitorialTecnico", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TipoServicio",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Servicio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    precio = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoServicio", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persona",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellidos = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumeroTelefono = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    correo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Latitud = table.Column<float>(type: "real", nullable: true),
+                    Longitud = table.Column<float>(type: "real", nullable: true),
+                    ServicioId = table.Column<int>(type: "int", nullable: true),
+                    HistorialClienteId = table.Column<int>(type: "int", nullable: true),
+                    Registro = table.Column<int>(type: "int", nullable: true),
+                    Disponibilidad = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persona", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Persona_HistorialCliente_HistorialClienteId",
+                        column: x => x.HistorialClienteId,
+                        principalTable: "HistorialCliente",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Servicio",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaHora = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Valor = table.Column<float>(type: "real", nullable: false),
+                    TipoServicioId = table.Column<int>(type: "int", nullable: true),
+                    TecnicoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Servicio", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Servicio_Persona_TecnicoId",
+                        column: x => x.TecnicoId,
+                        principalTable: "Persona",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Servicio_TipoServicio_TipoServicioId",
+                        column: x => x.TipoServicioId,
+                        principalTable: "TipoServicio",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persona_HistorialClienteId",
+                table: "Persona",
+                column: "HistorialClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persona_ServicioId",
+                table: "Persona",
+                column: "ServicioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Servicio_TecnicoId",
+                table: "Servicio",
+                column: "TecnicoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Servicio_TipoServicioId",
+                table: "Servicio",
+                column: "TipoServicioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Persona_Servicio_ServicioId",
+                table: "Persona",
+                column: "ServicioId",
+                principalTable: "Servicio",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Persona_HistorialCliente_HistorialClienteId",
+                table: "Persona");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Persona_Servicio_ServicioId",
+                table: "Persona");
+
+            migrationBuilder.DropTable(
+                name: "HitorialTecnico");
+
+            migrationBuilder.DropTable(
+                name: "HistorialCliente");
+
+            migrationBuilder.DropTable(
+                name: "Servicio");
+
+            migrationBuilder.DropTable(
+                name: "Persona");
+
+            migrationBuilder.DropTable(
+                name: "TipoServicio");
+        }
+    }
+}
